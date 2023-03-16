de06380f4824099b12f5dd66759c373344334019
gilbertmutaikiprono <mutaikipronogilbert@gmail.com>
2023-03-16 12:59:17 -0700

    C Corrections of Task 4 & 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /**
-* You should produce the exact same output as in the example
-* Warnings are allowed
-* 
-* Your program should return 0
-*/
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-        char c;
-        int i;
-        long li;
-        long long lli;
-        float f;
+		int a;
+		long int b;
+		long long int c;
+		char d;
+		float f;
 
-        printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of a int: %ld byte(s)\n", sizeof(i));
-	printf("size of a long: %ld byte(s)\n", sizeof(li));
-	printf("size of a long long: %ld byte(s)\n", sizeof(lli));
-	printf("size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+		return (0);
 }
