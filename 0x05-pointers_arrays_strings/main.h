11adf6d903f29d9e18cab6601a6919b031b73f23
gilbertmutaikiprono <mutaikipronogilbert@gmail.com>
2023-03-28 13:26:06 -0700

    Tasks

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
-#ifndef Main_H
-#define Main_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ */
 
-int_putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,7 +13,9 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
-#endif
+#endif /*MAIN_H*/
